ba90a6b5e9957379571f166548a4cd21ca22472d
duyn <khanhduy.dev@gmail.com>
2024-07-05 09:47:11 +0700

    fix build

--- src/pages/DownloadPage/DownloadPage.tsx
+++ src/pages/DownloadPage/DownloadPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './styles.module.scss';
-import Typography from '@/components/Typography';
+import Typography from '../../components/Typography';
 
 const cx = classNames.bind(styles);
 
